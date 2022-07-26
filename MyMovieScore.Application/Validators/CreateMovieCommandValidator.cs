4401808e8c73ec607dce28cd1046523a4fe0c617
João de Queiroz <joao.queiroz@modalgr.com.br>
2022-07-25 22:28:16 -0300

    feature: create validators for movies and config jwt in appsettings

---
+++ MyMovieScore.Application/Validators/CreateMovieCommandValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using MyMovieScore.Application.Commands.CreateMovie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMovieScore.Application.Validators
+{
+    public class CreateMovieCommandValidator : AbstractValidator<CreateMovieCommand>
+    {
+        public CreateMovieCommandValidator()
+        {
+            RuleFor(m => m.IdIMDb)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Should have IMDb ID");
+
+            RuleFor(m => m.Watched)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Should inform if was watched");
+
+            RuleFor(m => m.UserScore)
+                .GreaterThanOrEqualTo(0)
+                .LessThanOrEqualTo(10)
+                .WithMessage("Should inform if was watched");
+
+        }
+    }
+}
