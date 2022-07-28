34a6cb9d261aa53f1d39d009b309dc8aea3e598a
João de Queiroz <joao.queiroz@modalgr.com.br>
2022-07-28 01:01:12 -0300

    feature: incrise third party api call

--- MyMovieScore.Infrastructure/ExternalServices/IMDbExternalService.cs
+++ MyMovieScore.Infrastructure/ExternalServices/IMDbExternalService.cs
@@ -8,15 +8,26 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using MyMovieScore.Core.DTOs;
+using Microsoft.Extensions.Configuration;
 
 namespace MyMovieScore.Infrastructure.ExternalServices
 {
     public class IMDbExternalService : IIMDbExternalService
     {
+        private readonly IConfiguration _configuration;
+
+        public IMDbExternalService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public async Task<Movie> GetByIMDbIdAsync(string idIMDb)
         {
+            var value = _configuration.GetSection("ExternalService")["Key"];
+
+
             HttpClient client = new HttpClient { BaseAddress = new Uri("http://www.omdbapi.com") };
-            var response = await client.GetAsync($"?i={idIMDb}&apikey=1f936bec&plot=full");
+            var response = await client.GetAsync($"?i={idIMDb}&apikey={value}&plot=full");
             var content = await response.Content.ReadAsStringAsync();
             var movieDeserialize = JsonConvert.DeserializeObject<ImDbInforDto>(content);
             var movie = new Movie(
